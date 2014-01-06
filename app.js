a1bc89080e49f5a44094669c6bb0346fa9a6a03f
greenteawarrior <emily.wang@students.olin.edu>
2014-01-06 03:58:54 -0500

    got rid of duplicate code in app.js

--- app.js
+++ app.js
@@ -13,18 +13,6 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-//app configuration
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000); // sets up the port
-  app.set('views', __dirname + '/views'); // sets the path for views
-  app.set('view engine', 'jade'); // sets the engine that the views are rendered with
-  app.use(express.favicon()); // default favicon
-  app.use(express.logger('dev')); // error logging
-  app.use(express.bodyParser()); // 
-  app.use(express.methodOverride());
-  app.use(app.router); // 
-  app.use(express.static(path.join(__dirname, 'public'))); // sets the path for public files (css & js)
-});
 
 // all environments
 app.set('port', process.env.PORT || 5000);
